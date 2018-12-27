58f31ff669db750a12e01b876a5d8d0321c64cb8
Philippe Perret <philippe.perret@yahoo.fr>
2018-12-27 07:36:56 +0300

    Implémentation de la commande test

--- rakefile.rb
+++ rakefile.rb
@@ -5,7 +5,7 @@ require 'rake/testtask'
 Rake::TestTask.new do |t|
   # t.libs = ["lib"]|
   t.name = 'test:textanalyzer'
-  t.warning = true
-  t.verbose = true
-  t.test_files = FileList['test/text_analyzer/**/ts_*.rb']
+  t.warning     = true
+  t.verbose     = true
+  t.test_files  = FileList['test/text_analyzer/**/ts_*.rb']
 end
